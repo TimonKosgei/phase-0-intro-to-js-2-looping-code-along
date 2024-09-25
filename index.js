c8ea29acc0feeabeda4626fd6579dcfa397f4776
TimonKosgei <timonkosgei15209@gmail.com>
2024-09-25 11:27:38 +0300

    Did labs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(a,b){
+    let messages  = [];
+    for (let i = 0; i < a.length; i++){
+        let message = `Thank you, ${a[i]}, for the wonderful ${b} gift!`;
+        messages.push(message)
+    }
+    return messages;
+}
+function countDown(int){
+    while(int > -1){
+        console.log(int);
+        int--;
+    }
+}
